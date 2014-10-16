35d1a633ec1101a3f17a435f70caa8011cf2d41c
Henning Leutz <leutz@pcsg.de>
2014-10-16 12:28:06 +0200

    calendar -> new bin urls

--- bin/Calendar.js
+++ bin/Calendar.js
@@ -1,19 +1,19 @@
 /**
  * Datepicker for QUIQQER
  *
+ * @module URL_OPT_DIR/quiqqer/calendar/bin/Calendar
+ *
  * @author Arian Stolwijk (MooTools Datepicker)
  * @author www.pcsg.de (Henning Leutz - QUIQQER / AMD / moofx adaptation )
- *
- * @module package/quiqqer/calendar/bin/Calendar
  */
 
-define('package/quiqqer/calendar/bin/Calendar', [
+define([
 
-    'package/quiqqer/calendar/bin/Source/Picker',
-    'package/quiqqer/calendar/bin/Source/Picker.Attach',
-    'package/quiqqer/calendar/bin/Source/Picker.Date',
+    'URL_OPT_DIR/quiqqer/calendar/bin/Source/Picker',
+    'URL_OPT_DIR/quiqqer/calendar/bin/Source/Picker.Attach',
+    'URL_OPT_DIR/quiqqer/calendar/bin/Source/Picker.Date',
 
-    'css!package/quiqqer/calendar/bin/Source/datepicker_dashboard/datepicker_dashboard.css'
+    'css!URL_OPT_DIR/quiqqer/calendar/bin/Source/datepicker_dashboard/datepicker_dashboard.css'
 
 ], function(Picker, Attach, DatePicker)
 {
