c1b5c3d804ff8ce56c1bd1f3533968cb0dcd396e
Henning Leutz <leutz@pcsg.de>
2016-11-18 11:22:15 +0100

    refactor: start calendar classes

---
+++ src/QUI/Calendar/Calendar.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file contains QUI\Calendar\Calendar
+ */
+namespace QUI\Calendar;
+
+/**
+ * Class Calendar
+ * one Calendar
+ *
+ * @package QUI\Calendar
+ */
+class Calendar
+{
+    public function __construct($calendarId)
+    {
+
+    }
+
+    public function addCalendarEvent()
+    {
+
+    }
+
+    public function editCalendarEvent()
+    {
+
+    }
+
+    public function removeCalendarEvent()
+    {
+
+    }
+
+    public function toICal()
+    {
+        
+    }
+}
---
+++ src/QUI/Calendar/Handler.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file contains QUI\Calendar\Handler
+ */
+namespace QUI\Calendar;
+
+/**
+ * Class Handler
+ * @package QUI\Calendar
+ */
+class Handler
+{
+    /**
+     * Creates a new Calendar
+     *
+     * @param string $name - Calendar name
+     * @param null $User - optional, User for which the calendar is
+     */
+    public static function createCalendar($name, $User = null)
+    {
+
+    }
+}
