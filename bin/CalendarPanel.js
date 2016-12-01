cd413b1c8b589588988824d6e58623641924f8fa
Jan Wennrich <jan@pcsg.de>
2016-12-01 16:15:15 +0100

    feat: Display events in calendar

--- bin/CalendarPanel.js
+++ bin/CalendarPanel.js
@@ -44,22 +44,24 @@ define('package/quiqqer/calendar/bin/CalendarPanel', [
 
         $onCreate: function ()
         {
+            console.log('CID: ' + this.calendarID);
+
             var Content = this.getContent();
             Content.set({
                 html: Mustache.render(template)
             });
 
             Scheduler.init(Content.getElementById('scheduler_here'));
-            var events = [
-                {id:1, text:"Meeting",   start_date:"04/11/2013 14:00",end_date:"04/11/2013 17:00"},
-                {id:2, text:"Conference",start_date:"04/15/2013 12:00",end_date:"04/18/2013 19:00"},
-                {id:3, text:"Interview", start_date:"04/24/2013 09:00",end_date:"04/24/2013 10:00"}
-            ];
 
-            Scheduler.parse(events, 'json');
-//            Scheduler.addEvent('onEventChanged', function() {
-//                console.log('event changed.');
-//            })
+            QUIAjax.get('package_quiqqer_calendar_ajax_getCalendarAsIcal', function (result)
+            {
+                console.log(result);
+                Scheduler.config.full_day = true;
+                Scheduler.parse(result, 'ical');
+            }, {
+                'package' : 'quiqqer/calendar',
+                calendarID: this.calendarID
+            });
         },
 
         /**
