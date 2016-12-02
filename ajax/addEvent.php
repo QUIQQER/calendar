09dd4fdf655b7b88697c166f0bc204a8d25eb220
Jan Wennrich <jan@pcsg.de>
2016-12-02 12:23:02 +0100

    refactor: Removed try/catch to prevent leaking errors

--- ajax/addEvent.php
+++ ajax/addEvent.php
@@ -7,16 +7,12 @@
 QUI::$Ajax->registerFunction(
     'package_quiqqer_calendar_ajax_addEvent',
     function ($calendarID, $title, $desc, $start, $end) {
-        try {
-            if (is_null($calendarID) || empty($calendarID)) {
-                return null;
-            }
-            $Calendar = new \QUI\Calendar\Calendar((int) $calendarID);
-            $eventID = $Calendar->addCalendarEvent($title, $desc, $start, $end);
-            return (int) $eventID;
-        } catch (\Exception $ex) {
-            return "Error!: " . $ex->getMessage();
+        if (is_null($calendarID) || empty($calendarID)) {
+            return null;
         }
+        $Calendar = new \QUI\Calendar\Calendar((int) $calendarID);
+        $eventID = $Calendar->addCalendarEvent($title, $desc, $start, $end);
+        return (int) $eventID;
     },
     array('calendarID', 'title', 'desc', 'start', 'end'),
     'Permission::checkAdminUser'
