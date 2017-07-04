00cadb520c930cc372ef7b3231c8a02059f6324a
Panni <jan@pcsg.de>
2017-07-04 13:07:34 +0200

    feat: Calendar Display Brick (#11)

---
+++ src/QUI/Calendar/Bricks/CalendarDisplay.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file contains QUI\Calendar\Bricks\CalendarDisplay
+ */
+
+namespace QUI\Calendar\Bricks;
+
+use QUI;
+
+/**
+ * Class CalendarDisplay
+ *
+ * @package QUI\Calendar
+ * @author www.pcsg.de (Jan Wennrich)
+ */
+class CalendarDisplay extends QUI\Control
+{
+    public function getBody()
+    {
+        $height = '85vh';
+        if ($this->getAttribute('height')) {
+            $height = $this->getAttribute('height');
+        }
+
+        $this->setStyles([
+            'height' => $height
+        ]);
+
+        $this->setJavaScriptControl('package/quiqqer/calendar/bin/controls/CalendarDisplay');
+        $this->setJavaScriptControlOption('calendarids', $this->getAttribute('calendarIDs'));
+
+        $this->setAttribute('class', 'quiqqer-calendar-brick-calendar-display');
+
+        return;
+    }
+}
