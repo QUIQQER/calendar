4ce23fb54db5bee9263ff378c74b5d36316f7599
Panni <jan@pcsg.de>
2017-08-02 11:30:26 +0200

    feat: Added Cron to sync external calendars; closes #8

---
+++ src/QUI/Calendar/Cron.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file contains \QUI\Calendar\Cron
+ */
+
+namespace QUI\Calendar;
+
+use QUI;
+
+/**
+ * Class Cron / Calendar Crons
+ *
+ * @package quiqqer/log
+ * @author  www.pcsg.de (Jan Wennrich)
+ */
+class Cron
+{
+    /**
+     * Syncs all external calendars from their origin with the QUIQQER system
+     *
+     * @param array $params
+     * @param \QUI\Cron\Manager $CronManager
+     */
+    public static function syncExternalCalendars($params, $CronManager)
+    {
+        foreach (Handler::getExternalCalendars() as $Calendar) {
+            // Loads non cached calendars into cache
+            try {
+                $Calendar->getIcalData();
+            } catch (Exception $exception) {
+                continue;
+            }
+        }
+    }
+}
