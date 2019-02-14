dcda5afd53f05094d9e19ebdd5b5265848a45cab
Jan Wennrich <jan.wennrich@pcsg.de>
2019-02-14 11:24:02 +0100

    refactor: updated (weird) methods to use newest features

--- src/QUI/Calendar/EventManager.php
+++ src/QUI/Calendar/EventManager.php
@@ -2,6 +2,8 @@
 
 namespace QUI\Calendar;
 
+use QUI\Calendar\Exception\NoPermission;
+
 class EventManager
 {
     /**
@@ -42,31 +44,17 @@ class EventManager
      *
      * @param int $id - ID of a calendar of which upcoming events should be retrieved
      * @param bool $limit - Maximum amount of events to get
+     *
      * @return Event[] - An array of upcoming events
+     *
+     * @throws NoPermission - Current user isn't allowed to view the calendar
+     * @throws \QUI\Exception - Calendar with the given ID doesn't exist or is not accessible
+     *
+     * @deprecated - Use getUpcomingEvents() of a AbstractCalendar instance instead
      */
     public static function getUpcomingEventsForCalendarId($id, $limit = false)
     {
-        Handler::getCalendar($id)->checkPermission(AbstractCalendar::PERMISSION_VIEW_CALENDAR);
-
-        $eventsRaw = \QUI::getDataBase()->fetch(array(
-            'from'  => Handler::tableCalendarsEvents(),
-            'where' => array(
-                'calendarid' => $id,
-                'start'      => array(
-                    'value' => time(),
-                    'type'  => '>='
-                )
-            ),
-            'limit' => $limit,
-            'order' => 'start'
-        ));
-
-        $events = array();
-        foreach ($eventsRaw as $eventData) {
-            $events[] = Event::fromDatabaseArray($eventData);
-        }
-
-        return $events;
+        return Handler::getCalendar($id)->getUpcomingEvents($limit);
     }
 
 
@@ -75,11 +63,14 @@ class EventManager
      *
      * @param AbstractCalendar $Calendar - Calendar of which upcoming events should be retrieved
      * @param bool $limit - Maximum amount of events to get
+     *
      * @return Event[] - An array of upcoming events
+     *
+     * @deprecated - Use getUpcomingEvents() of a AbstractCalendar instance instead
      */
     public function getUpcomingEventsForCalendar(AbstractCalendar $Calendar, $limit = false)
     {
-        return $this->getUpcomingEventsForCalendarId($Calendar->getId(), $limit);
+        return $Calendar->getUpcomingEvents($limit);
     }
 
 
@@ -88,50 +79,30 @@ class EventManager
      *
      * @param int[] $ids - Array of calendar IDs of which upcoming events should be retrieved
      * @param int|bool $limit - Maximum amount of events to get
+     *
      * @return Event[] - An array of upcoming events
      */
     public static function getUpcomingEventsForCalendarIds(array $ids, $limit = false)
     {
-        foreach ($ids as $key => $calendarID) {
+        /** @var Event[] $events */
+        $events = [];
+        foreach ($ids as $calendarID) {
             try {
-                Handler::getCalendar($calendarID)->checkPermission(AbstractCalendar::PERMISSION_VIEW_CALENDAR);
-            } catch (Exception $exception) {
-                // The calendar does not exist or user doesn't have permission to view it -> remove calendar ID
-                unset($ids[$key]);
+                $calendarsEvents = Handler::getCalendar($calendarID)->getUpcomingEvents($limit);
+                $events          = array_merge($events, $calendarsEvents);
+            } catch (\QUI\Exception $exception) {
                 continue;
             }
         }
 
-        $table = Handler::tableCalendarsEvents();
+        usort($events, function ($EventA, $EventB) {
+            /** @var Event $EventA */
+            /** @var Event $EventB */
+            return strcmp($EventA->start_date, $EventB->start_date);
+        });
 
-        $ids = implode(', ', $ids);
-
-        $sql = "
-            SELECT * 
-            FROM {$table}
-            WHERE 
-              calendarid IN ({$ids}) AND 
-              start >= :startDate
-            ORDER BY start ASC
-        ";
-
-        $parameters = array(
-            ':startDate' => time()
-        );
-
-        if (is_numeric($limit) && $limit > 0) {
-            $limit = (int)$limit;
-            $sql   .= "LIMIT {$limit}";
-        }
-
-        $Statement = \QUI::getDataBase()->getPDO()->prepare($sql);
-        $Statement->execute($parameters);
-
-        $eventsRaw = $Statement->fetchAll(\PDO::FETCH_ASSOC);
-
-        $events = array();
-        foreach ($eventsRaw as $eventData) {
-            $events[] = Event::fromDatabaseArray($eventData);
+        if (is_int($limit) && $limit > 0) {
+            $events = array_slice($events, 0, $limit);
         }
 
         return $events;
@@ -147,7 +118,7 @@ class EventManager
      */
     public static function getUpcomingEventsForCalendars(array $calendars, $limit = false)
     {
-        $ids = array();
+        $ids = [];
         foreach ($calendars as $Calendar) {
             $ids[] = $Calendar->getId();
         }
