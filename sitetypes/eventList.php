1dedba12238292c908b4f5eca8143eb6800a7e59
michael.daniel <michael@pcsg.de>
2019-06-18 01:14:13 +0200

    feat: Design for events in the past, future and now. Calendar name added.

--- sitetypes/eventList.php
+++ sitetypes/eventList.php
@@ -22,11 +22,23 @@ foreach ($calendarIDs as $calendarID) {
     }
 }
 
-
 \usort($events, function ($a, $b) {
     return \strtotime($a->start_date) - \strtotime($b->start_date);
 });
 
+foreach ($events as $event) {
+    $eventDate = new DateTime($event->start_date);
+
+    if ($eventDate->format('Y-m-d') < $currDay->format('Y-m-d')) {
+        $event->eventTimeStatus = 'past';
+    } else {
+        if ($eventDate->format('Y-m-d') > $currDay->format('Y-m-d')) {
+            $event->eventTimeStatus = 'future';
+        } else {
+            $event->eventTimeStatus = 'now';
+        }
+    }
+}
 
 $Engine->assign([
     'events'  => $events,
