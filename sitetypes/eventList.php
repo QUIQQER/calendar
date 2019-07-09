56b0783032651e2d460227bd18dfda2024036233
michael.daniel <michael@pcsg.de>
2019-07-09 17:03:59 +0200

    feat: If no event today outputs corresponding message.

--- sitetypes/eventList.php
+++ sitetypes/eventList.php
@@ -26,26 +26,55 @@ foreach ($calendarIDs as $calendarID) {
     return \strtotime($a->start_date) - \strtotime($b->start_date);
 });
 
+$counter          = 0;
+$incrementCounter = true;
+$todayEvent       = false;
+
 foreach ($events as $event) {
     $eventDate = new \DateTime($event->start_date);
 
+    // bad code...
     if ($eventDate->format('Y-m-d') < $currDay->format('Y-m-d')) {
         $event->eventTimeStatus = 'past';
     } else {
-        if ($eventDate->format('Y-m-d') > $currDay->format('Y-m-d')) {
-            $event->eventTimeStatus = 'future';
-        } else {
+        if ($eventDate->format('Y-m-d') == $currDay->format('Y-m-d')) {
             $event->eventTimeStatus = 'now';
+            $todayEvent             = true;
+            $incrementCounter       = false;
+        } else {
+            if (!$todayEvent) {
+                $incrementCounter = false;
+            }
+
+            $event->eventTimeStatus = 'future';
         }
     }
+
+    if ($incrementCounter) {
+        $counter++;
+    }
 }
+
+// no event today?
+if ($counter) {
+    $EmptyEvent = new QUI\Calendar\Event(
+        'Kein besonderer Tag heute',
+        'Heute gibt es nichts, was besonders ist.',
+        $currDay->format('Y-m-d'),
+        $currDay->format('Y-m-d')
+    );
+
+    $EmptyEvent->eventTimeStatus = 'now';
+    $toInsert                    = [$EmptyEvent];
+
+    array_splice($events, $counter, 0, $toInsert);
+}
+
 
 $Engine->assign([
     'events'  => $events,
     'currDay' => $currDay
 ]);
-
-echo $Site->getAttribute('calendar.settings.template');
 
 switch ($Site->getAttribute('calendar.settings.template')) {
     case 'timeline':
