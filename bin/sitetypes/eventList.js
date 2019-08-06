d11ca6bf78c3c64a4811f4a2ce3b0a36265c064f
michael.daniel <michael@pcsg.de>
2019-08-06 16:51:48 +0200

    feat: forgotten file.

---
+++ bin/sitetypes/eventList.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+
+    require(['qui/QUI'], function (QUI) {
+        // do not scroll immediately
+        (function () {
+            scrollToCurrentEvent(QUI);
+        }).delay(1000);
+    });
+
+    /**
+     * Scroll to current (today's) event
+     */
+    function scrollToCurrentEvent (QUI) {
+        var CurrentEventElm = document.getElement('.quiqqer-eventList-entry.status-now'),
+            pos             = CurrentEventElm.getPosition().y + 100, // offset 100px
+            winHeight       = QUI.getWindowSize().y,
+            bodyScroll      = QUI.getScroll().y;
+
+        if (pos < winHeight + bodyScroll) {
+            return;
+        }
+
+        if (CurrentEventElm) {
+            new Fx.Scroll(window, {
+                offset: {
+                    x: 0,
+                    y: pos - 50 - (winHeight / 2)
+                }
+            }).toTop();
+        }
+    }
+});
