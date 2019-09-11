407690153142f6e4cde8f1ce56876d243f785d8d
Jan Wennrich <jan.wennrich@pcsg.de>
2019-09-11 12:07:40 +0200

    feat: reloads calendar-contextmenu after adding a new calendar (#50)

--- src/QUI/Calendar/QuiqqerEventHandler.php
+++ src/QUI/Calendar/QuiqqerEventHandler.php
@@ -6,6 +6,6 @@ class QuiqqerEventHandler
 {
     public static function onAdminLoadFooter()
     {
-        echo "<script src='" . URL_OPT_DIR . "quiqqer/calendar/bin/initMenuEntries.js'></script>";
+        echo "<script src='" . URL_OPT_DIR . "quiqqer/calendar/bin/initCalendarMenuEntries.js'></script>";
     }
 }
